dcabb92fc9d9606fa269338add336cafabecdb8c
Hampus Mann <hampus.mann@learnet.se>
2019-01-24 10:52:49 +0100

    klar

---
+++ src/game.java
@@ -0,0 +1,161 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class game {
+    public static void main(String[] args) {
+
+        File file = new File("words.txt");
+        Random rand = new Random();
+        int n = rand.nextInt(500);
+        Scanner fileInput = null;
+
+        try {
+            fileInput = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> possibleWords = new ArrayList<>();
+        while (fileInput.hasNextLine()){
+            possibleWords.add(fileInput.nextLine());
+        }
+
+        String[] wordsPossible = new String[possibleWords.size()];
+        for (int i = 0 ; i < possibleWords.size() ; i++)  {
+            wordsPossible[i] = possibleWords.get(i);
+        }
+
+        String word = wordsPossible[n];
+
+        char[] wordd = new char[word.length()];
+        char[] antal = new char[word.length()];
+
+        for (int i = 0 ; i < word.length() ; i++) {
+            wordd[i] = word.charAt(i);
+            antal[i] = '_';
+        }
+
+        UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Courier New", Font.BOLD, 18)));
+
+        String[] stage = {
+                "       \n" +
+                        "       \n" +
+                        "       \n" +
+                        "       \n" +
+                        "       \n" +
+                        "       \n" +
+                        "=========" ,
+                "       \n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "    --+\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                "  |   |\n" +
+                "  O   |\n" +
+                "      |\n" +
+                "      |\n" +
+                "      |\n" +
+                "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        "  |   |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|   |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|\\  |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|\\  |\n" +
+                        " /    |\n" +
+                        "      |\n" +
+                        "=========" ,
+                "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|\\  |\n" +
+                        " / \\  |\n" +
+                        "      |\n" +
+                        "========="
+        };
+
+        int s = 0;
+        char gissning;
+        String word2 = new String(antal);
+        int ok = 0;
+
+        int chars = word.length();
+        ArrayList<Character> used = new ArrayList<>();
+        ArrayList<Character> wrong = new ArrayList<>();
+        JOptionPane.showMessageDialog(null, "Hangman Game 1.0");
+        JOptionPane.showMessageDialog(null, "The word contains " + chars + " characters");
+
+        while (ok == 0) {
+            gissning = JOptionPane.showInputDialog(null, stage[s] + "\n" + word2 + "\n" + "Wrong guesses == " + wrong + "\n\nGuess a character").charAt(0);
+            for (int i = 0 ; i < word.length() ; i++) {
+                 if (used.contains(gissning)) {
+                     JOptionPane.showMessageDialog(null, "This character has already been guessed");
+                     break;
+                 }
+                 if (gissning == wordd[i]) {
+                    antal[i] = wordd[i];
+                }
+            }
+            String gis = "" + gissning;
+            if (!word.contains(gis)) {
+                if (!wrong.contains(gissning)){
+                    wrong.add(gissning);
+                    s = s + 1;
+                }
+            }
+            word2 = new String(antal);
+
+            used.add(gissning);
+            if (word2.equals(word)){
+                ok = 1;
+                JOptionPane.showMessageDialog(null, "You guessed right, the word was " + word + ", congratulations!");
+            } if (s == 9){
+                JOptionPane.showMessageDialog(null, "You have no more guesses left, sad. \nThe word was " + word);
+                break;
+            }
+        }
+
+    }
+}
